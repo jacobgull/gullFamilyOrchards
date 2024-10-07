69ae93f21f966bb9ad01edf997ccccf25b06db20
Jacob Gull <jacobgull@Jacobs-MacBook-Pro-2.local>
2024-10-07 14:52:06 -0700

    Added privacy policy and osano

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
+import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: 'privacy', component: PrivacyPolicyComponent }
   ];
